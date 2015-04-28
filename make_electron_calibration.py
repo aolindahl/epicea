af8e44eae2151be67cd7998b811f447d347d5891
Anton O. Lindahl <aolindahl@gmail.com>
2015-04-28 18:44:09 +0200

    Kr and N calibration
    Handle single and double lines in the calibration. Catch
    CenterEnergyError.

--- make_electron_calibration.py
+++ make_electron_calibration.py
@@ -14,54 +14,93 @@ if 'epicea' not in locals():
 
 
 def make_calibration(setting, verbose=False):
+    """Make the electron calibration for a given center energy setting."""
+    # The Kr binding energies are needed for Kr based calibrations
     kr_binding_energies = np.array([93.788, 95.038])
+    n2_binidng_energy = np.array([409.9])
 
-    calib_data_list, photon_energy_dict = \
-        electron_calibration_data.get_data_in_list(setting,
-                                                   verbose=verbose)
-
+    # Get the list of data sets
+    try:
+        calib_data_list = electron_calibration_data.get_data_in_list(
+            setting, verbose=verbose)
+    except electron_calibration_data.CenterEnergyError as e:
+        print e
+        return
+    # and make sure the list is sorted
     calib_data_list.sort(reverse=True)
 
+    if 'Kr' in calib_data_list[0].name():
+        gas = 'Kr'
+        binding = kr_binding_energies
+    elif 'N2' in calib_data_list[0].name():
+        gas = 'N2'
+        binding = n2_binidng_energy
+    else:
+        print 'Unknown gas.',
+        print 'Dataset names must specify the used gas (N2 or Kr).'
+
+    n_lines = len(binding)
+
+    # Create an emptu calibration object
     calibration = epicea.ElectronEnergyCalibration()
 
-#     Need to be done only once
-    for c_data in calib_data_list:
-        c_data.electrons.recalculate_polar_coordinates()
-
+    # Create a plot of all the spectra in the data list
     print 'Plot all the raw spectra.'
-    x_axis_mm = np.linspace(-20, 20, 512)
-    plt_func.figure_wrapper('Kr e spectra')
+    # Make an x axis
+    x_axis_mm = np.linspace(-23, 23, 512)
+    # Create the figure
+    plt_func.figure_wrapper('Calibration spectra {} eV'.format(setting))
+    # One subplot for each data set...
     n_subplots = calib_data_list.len()
+    # ... nicely set in collumns and rows
     n_rows = np.floor(np.sqrt(n_subplots))
     n_cols = np.ceil(float(n_subplots)/n_rows)
+    # Iterate over the datasets
     for i, c_data in enumerate(calib_data_list):
-        plt.subplot(n_rows, n_cols, i+1)
+        plt.subplot(n_rows, n_cols, i+1)  # Make the subplot
+        # Show the electron figure
         plt_func.imshow_wrapper(c_data.get_e_xy_image(x_axis_mm), x_axis_mm)
+        # Adjust the figure for good looks
         plt_func.tick_fontsize()
         plt_func.title_wrapper(c_data.name())
         plt_func.xlabel_wrapper('Position (mm)')
         plt_func.ylabel_wrapper('Position (mm)')
 
+    plt.tight_layout()
+    # Define polar coordinate axis vectors
     r_axis_mm = np.linspace(0, 25, 257)[1::2]
     th_axis_rad = np.linspace(0, 2*np.pi, 257)[1::2]
 
+    # Iterate over the datasets and make images for each set.
+    # Also find the lines in each of the spectra
     for i, c_data in enumerate(calib_data_list):
         print 'Make the theta-r spectrum for {}.'.format(c_data.name())
+        # Make the figure
         plt_func.figure_wrapper(
-            'Kr e spectra theta-r {}'.format(c_data.name()))
+            'e spectra theta-r {}'.format(c_data.name()))
+        # Get the image fron the data in polar coordinates
         e_rth_image = c_data.get_e_rth_image(r_axis_mm, th_axis_rad)
+        # Show the image
         plt_func.imshow_wrapper(e_rth_image, r_axis_mm, th_axis_rad,
                                 kw_args={'aspect': 'auto'})
+        # Make the image look nice
         plt_func.tick_fontsize()
         plt_func.title_wrapper(c_data.name())
         plt_func.xlabel_wrapper('Position (mm)')
         plt_func.ylabel_wrapper('Angle (rad)')
 
-        print 'Find the Kr 3d lines in {}.'.format(c_data.name())
-        r_1, w_1, r_2, w_2, red_chi2 = epicea.find_kr_lines(
-            e_rth_image, r_axis_mm, th_axis_rad)
-        plt.errorbar(r_1, th_axis_rad, xerr=w_1, fmt='.r', capsize=0)
-        plt.errorbar(r_2, th_axis_rad, xerr=w_2, fmt='.m', capsize=0)
+        if gas == 'Kr':
+            print 'Find the Kr 3d lines in {}.'.format(c_data.name())
+        elif gas == 'N2':
+            print 'Find the N_2 s1 line in {}.'.format(c_data.name())
+        r, w, red_chi2 = epicea.find_lines(
+            e_rth_image, r_axis_mm, th_axis_rad, n_lines=n_lines)
+        fmts = ['.r', '.m', '.y']
+        for line in range(n_lines):
+            plt.errorbar(r[:, line], th_axis_rad, xerr=w[:, line],
+                         fmt=fmts[line], capsize=1)
+#        plt.errorbar(r_1, th_axis_rad, xerr=w_1, fmt='.r', capsize=0)
+#        plt.errorbar(r_2, th_axis_rad, xerr=w_2, fmt='.m', capsize=0)
 
 #        poly_order = 40
 #        l1_params = line_start_params([0]*poly_order)
@@ -72,27 +111,24 @@ def make_calibration(setting, verbose=False):
 #                             args=(th_axis_rad, r_2, w_2))
 #        plt.plot(poly_line(l2_params, th_axis_rad), th_axis_rad, 'k')
 
-        print 'Lineoiuts in waterfall plot.'
-        plt_func.figure_wrapper('lineouts {}'.format(c_data.name()))
-        for i, line in enumerate(e_rth_image):
-            plt.plot(r_axis_mm, line + i*20, 'b')
+#        print 'Lineoiuts in waterfall plot.'
+#        plt_func.figure_wrapper('lineouts {}'.format(c_data.name()))
+#        for i, line in enumerate(e_rth_image):
+#            plt.plot(r_axis_mm, line + i*20, 'b')
 
         print 'Plot reduced chi^2.'
         plt_func.figure_wrapper('reduced chi^2 {}'.format(c_data.name()))
         plt.plot(red_chi2, th_axis_rad)
 
         print 'Add data to the calibration object'
-        calibration.add_calibration_data(
-            r_1, th_axis_rad,
-            photon_energy_dict[c_data.name()] - kr_binding_energies[0],
-            red_chi2)
-        calibration.add_calibration_data(
-            r_2, th_axis_rad,
-            photon_energy_dict[c_data.name()] - kr_binding_energies[1],
-            red_chi2)
+        for line in range(n_lines):
+            calibration.add_calibration_data(
+                r[:, line], th_axis_rad,
+                c_data.photon_energy() - binding[line],
+                w[:, line])
 
     print 'Create calibration'
-    calibration.create_conversion(poly_order=2)
+    calibration.create_conversion(poly_order=1)
 
     print 'Check the calibration'
     plt_func.figure_wrapper('Calib data check')
@@ -106,12 +142,13 @@ def make_calibration(setting, verbose=False):
                  epicea.poly_line(calibration._energy_params_list[idx],
                                   r_axis_for_calib_check_mm), 'r')
 
-    E_axis_eV = np.linspace(360, 380, 257)[1::2]
+    E_axis_eV = np.linspace(setting-30, setting+20, 2**8+1)[1::2]
     E_all = []
     err_all = []
     theta_all = []
     for c_data in calib_data_list:
-        print 'Get the calibrated energies.'
+        print 'Get the calibrated energies, {} eV.'.format(
+            c_data.photon_energy())
         E, err = calibration.get_energies(c_data.electrons.pos_r,
                                           c_data.electrons.pos_t)
         E_all.extend(E)
@@ -128,7 +165,7 @@ def make_calibration(setting, verbose=False):
 
 
 if __name__ == '__main__':
-    for setting in ['373']:
+    for setting in [500]:
         print 'Procesing calibration data for the {} eV setting.'.format(
             setting)
         make_calibration(setting, verbose=True)
